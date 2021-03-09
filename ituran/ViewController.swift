062b749fb55ed5470026d3df6b2c2aae8112a327
aviadr94 <aviadr94@gmail.com>
2021-03-09 11:11:59 +0200

    Initial Commit

---
+++ ituran/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ituran
+//
+//  Created by אביעד on 09/03/2021.
+//  Copyright © 2021 Shiran Rahamim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
